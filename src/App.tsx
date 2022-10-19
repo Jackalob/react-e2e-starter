7f2a68e53719b596df8b3ced865585dcbb99fda0
Jackalob <nike9712@gmail.com>
2022-10-20 00:55:17 +0800

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
-function App() {
+type Todo = {
+  id: number;
+  title: string;
+  selected: boolean;
+};
+
+export default function App() {
+  const [authenticated, setAuthenticated] = useState(true);
+  const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const idRef = useRef(1);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (todoInput === "") return;
+    
+    setTimeout(() => {
+      setTodos((todos) => [
+        ...todos,
+        {
+          id: idRef.current,
+          title: todoInput,
+          selected: false,
+        },
+      ]);
+      idRef.current += 1;
+    }, 500);
+    setTodoInput("");
+  };
+
+  const toggleTodo = (id: number) => {
+    const filteredTodo = todos.find((todo) => todo.id === id);
+    if (filteredTodo) {
+      filteredTodo.selected = !filteredTodo.selected;
+    }
+    const newTodos = todos.map((todo) =>
+      todo.id === id ? filteredTodo! : todo
+    );
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!authenticated && (
+        <>
+          <h1>Login</h1>
+          <button type="button" onClick={() => setAuthenticated(true)}>
+            Click here to login
+          </button>
+        </>
+      )}
+
+      {authenticated && (
+        <>
+          <h1>Add Todo</h1>
+          <form onSubmit={onSubmit}>
+            <label htmlFor="title">Title</label>
+            <br />
+            <input
+              type="text"
+              id="title"
+              value={todoInput}
+              onChange={(e) => setTodoInput(e.target.value)}
+            />
+            <br />
+            <button type="submit">Submit</button>
+          </form>
+
+          <ul>
+            {todos.map((todo) => {
+              const { id, title, selected } = todo;
+              return (
+                <li key={id}>
+                  <label
+                    className={selected ? "todoText" : ""}
+                    htmlFor={`todoToggle${id}`}
+                  >
+                    {title}
+                  </label>
+                  <input
+                    type="checkbox"
+                    id={`todoToggle${id}`}
+                    onChange={() => toggleTodo(id)}
+                  />
+                  <button type="button" onClick={() => deleteTodo(id)}>
+                    Remove
+                  </button>
+                  <br />
+                </li>
+              );
+            })}
+          </ul>
+
+          <p>Total Todos: {todos.length}</p>
+          <p>Selected Todos: {todos.filter((todo) => todo.selected).length}</p>
+        </>
+      )}
+    </>
   );
 }
-
-export default App;
